e536f4dfe58eebf826ae78025030811abd450c07
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2022-02-03 03:07:53 +0300

    first commit

--- routes/upload.js
+++ routes/upload.js
@@ -2,15 +2,15 @@
 
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").load();
+    require("dotenv").load();
 }
 
 const msRest = require("@azure/ms-rest-js");
 const Face = require("@azure/cognitiveservices-face");
 const { uuid } = require("uuidv4");
 
-const key = "f7434d19e370476da51b9405e73b47eb";
-const endpoint = "https://fcetrial.cognitiveservices.azure.com/";
+const key = "e53f290b7a3c46c49daa935adc4cb96f";
+const endpoint = "https://face-rg.cognitiveservices.azure.com/";
 
 // <credentials>
 const credentials = new msRest.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } });
@@ -18,32 +18,35 @@ const client = new Face.FaceClient(credentials, endpoint);
 // </credentials>
 
 // <globals>
-const image_base_url = "https://sakardesler.blob.core.windows.net/images/";
-const source_image_base_url = "https://sakardesler.blob.core.windows.net/imagescheck/";
+
+
+
+const image_base_url = "https://sakardeser.blob.core.windows.net/images/";
+const source_image_base_url = "https://sakardeser.blob.core.windows.net/imagescheck/";
 const person_group_id = uuid();
 
 const express = require("express"),
-  router = express.Router(),
-  multer = require("multer"),
-  inMemoryStorage = multer.memoryStorage(),
-  uploadStrategy = multer({ storage: inMemoryStorage }).single("image"),
-  azureStorage = require("azure-storage"),
-  blobService = azureStorage.createBlobService(),
-  getStream = require("into-stream"),
-  containerName = "imagescheck";
+    router = express.Router(),
+    multer = require("multer"),
+    inMemoryStorage = multer.memoryStorage(),
+    uploadStrategy = multer({ storage: inMemoryStorage }).single("image"),
+    azureStorage = require("azure-storage"),
+    blobService = azureStorage.createBlobService(),
+    getStream = require("into-stream"),
+    containerName = "imagescheck";
 
 
 var iden = 1;
 
 
 const handleError = (err, res) => {
-  res.status(500);
-  res.render("error", { error: err });
+    res.status(500);
+    res.render("error", { error: err });
 };
 
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 // </helpers>
 
@@ -53,120 +56,114 @@ async function DetectFaceExtract() {
     console.log();
 
     // Create a list of images
-	const image_file_names = [
-		"detection1.jpg",    // single female with glasses
-		// "detection2.jpg", // (optional: single man)
-		// "detection3.jpg", // (optional: single male construction worker)
-		// "detection4.jpg", // (optional: 3 people at cafe, 1 is blurred)
-		"detection5.jpg",    // family, woman child man
-		"detection6.jpg"     // elderly couple, male female
-	];
+    const image_file_names = [
+        "detection1.jpg", // single female with glasses
+        // "detection2.jpg", // (optional: single man)
+        // "detection3.jpg", // (optional: single male construction worker)
+        // "detection4.jpg", // (optional: 3 people at cafe, 1 is blurred)
+        "detection5.jpg", // family, woman child man
+        "detection6.jpg" // elderly couple, male female
+    ];
 
-// NOTE await does not work properly in for, forEach, and while loops. Use Array.map and Promise.all instead.
-	await Promise.all (image_file_names.map (async function (image_file_name) {
-        let detected_faces = await client.face.detectWithUrl(image_base_url + image_file_name,
-			{
-				returnFaceAttributes: ["Accessories","Age","Blur","Emotion","Exposure","FacialHair","Gender","Glasses","Hair","HeadPose","Makeup","Noise","Occlusion","Smile","QualityForRecognition"],
-				// We specify detection model 1 because we are retrieving attributes.
-				detectionModel: "detection_01",
-                recognitionModel: "recognition_03"
-			});
-        console.log (detected_faces.length + " face(s) detected from image " + image_file_name + ".");
-		console.log("Face attributes for face(s) in " + image_file_name + ":");
+    // NOTE await does not work properly in for, forEach, and while loops. Use Array.map and Promise.all instead.
+    await Promise.all(image_file_names.map(async function(image_file_name) {
+        let detected_faces = await client.face.detectWithUrl(image_base_url + image_file_name, {
+            returnFaceAttributes: ["Accessories", "Age", "Blur", "Emotion", "Exposure", "FacialHair", "Gender", "Glasses", "Hair", "HeadPose", "Makeup", "Noise", "Occlusion", "Smile", "QualityForRecognition"],
+            // We specify detection model 1 because we are retrieving attributes.
+            detectionModel: "detection_01",
+            recognitionModel: "recognition_03"
+        });
+        console.log(detected_faces.length + " face(s) detected from image " + image_file_name + ".");
+        console.log("Face attributes for face(s) in " + image_file_name + ":");
 
-// Parse and print all attributes of each detected face.
-		detected_faces.forEach (async function (face) {
-			// Get the bounding box of the face
-			console.log("Bounding box:\n  Left: " + face.faceRectangle.left + "\n  Top: " + face.faceRectangle.top + "\n  Width: " + face.faceRectangle.width + "\n  Height: " + face.faceRectangle.height);
+        // Parse and print all attributes of each detected face.
+        detected_faces.forEach(async function(face) {
+            // Get the bounding box of the face
+            console.log("Bounding box:\n  Left: " + face.faceRectangle.left + "\n  Top: " + face.faceRectangle.top + "\n  Width: " + face.faceRectangle.width + "\n  Height: " + face.faceRectangle.height);
 
-			// Get the accessories of the face
-			let accessories = face.faceAttributes.accessories.join();
-			if (0 === accessories.length) {
-				console.log ("No accessories detected.");
-			}
-			else {
-				console.log ("Accessories: " + accessories);
-			}
+            // Get the accessories of the face
+            let accessories = face.faceAttributes.accessories.join();
+            if (0 === accessories.length) {
+                console.log("No accessories detected.");
+            } else {
+                console.log("Accessories: " + accessories);
+            }
 
-			// Get face other attributes
-			console.log("Age: " + face.faceAttributes.age);
-			console.log("Blur: " + face.faceAttributes.blur.blurLevel);
+            // Get face other attributes
+            console.log("Age: " + face.faceAttributes.age);
+            console.log("Blur: " + face.faceAttributes.blur.blurLevel);
 
-			// Get emotion on the face
-			let emotions = "";
-			let emotion_threshold = 0.0;
-			if (face.faceAttributes.emotion.anger > emotion_threshold) { emotions += "anger, "; }
-			if (face.faceAttributes.emotion.contempt > emotion_threshold) { emotions += "contempt, "; }
-			if (face.faceAttributes.emotion.disgust > emotion_threshold) { emotions +=  "disgust, "; }
-			if (face.faceAttributes.emotion.fear > emotion_threshold) { emotions +=  "fear, "; }
-			if (face.faceAttributes.emotion.happiness > emotion_threshold) { emotions +=  "happiness, "; }
-			if (face.faceAttributes.emotion.neutral > emotion_threshold) { emotions +=  "neutral, "; }
-			if (face.faceAttributes.emotion.sadness > emotion_threshold) { emotions +=  "sadness, "; }
-			if (face.faceAttributes.emotion.surprise > emotion_threshold) { emotions +=  "surprise, "; }
-			if (emotions.length > 0) {
-				console.log ("Emotions: " + emotions.slice (0, -2));
-			}
-			else {
-				console.log ("No emotions detected.");
-			}
-			
-			// Get more face attributes
-			console.log("Exposure: " + face.faceAttributes.exposure.exposureLevel);
-			if (face.faceAttributes.facialHair.moustache + face.faceAttributes.facialHair.beard + face.faceAttributes.facialHair.sideburns > 0) {
-				console.log("FacialHair: Yes");
-			}
-			else {
-				console.log("FacialHair: No");
-			}
-			console.log("Gender: " + face.faceAttributes.gender);
-			console.log("Glasses: " + face.faceAttributes.glasses);
+            // Get emotion on the face
+            let emotions = "";
+            let emotion_threshold = 0.0;
+            if (face.faceAttributes.emotion.anger > emotion_threshold) { emotions += "anger, "; }
+            if (face.faceAttributes.emotion.contempt > emotion_threshold) { emotions += "contempt, "; }
+            if (face.faceAttributes.emotion.disgust > emotion_threshold) { emotions += "disgust, "; }
+            if (face.faceAttributes.emotion.fear > emotion_threshold) { emotions += "fear, "; }
+            if (face.faceAttributes.emotion.happiness > emotion_threshold) { emotions += "happiness, "; }
+            if (face.faceAttributes.emotion.neutral > emotion_threshold) { emotions += "neutral, "; }
+            if (face.faceAttributes.emotion.sadness > emotion_threshold) { emotions += "sadness, "; }
+            if (face.faceAttributes.emotion.surprise > emotion_threshold) { emotions += "surprise, "; }
+            if (emotions.length > 0) {
+                console.log("Emotions: " + emotions.slice(0, -2));
+            } else {
+                console.log("No emotions detected.");
+            }
 
-			// Get hair color
-			var color = "";
-			if (face.faceAttributes.hair.hairColor.length === 0) {
-				if (face.faceAttributes.hair.invisible) { color = "Invisible"; } else { color = "Bald"; }
-			}
-			else {
-				color = "Unknown";
-				var highest_confidence = 0.0;
-				face.faceAttributes.hair.hairColor.forEach (function (hair_color) {
-					if (hair_color.confidence > highest_confidence) {
-						highest_confidence = hair_color.confidence;
-						color = hair_color.color;
-					}
-				});
-			}
-			console.log("Hair: " + color);
+            // Get more face attributes
+            console.log("Exposure: " + face.faceAttributes.exposure.exposureLevel);
+            if (face.faceAttributes.facialHair.moustache + face.faceAttributes.facialHair.beard + face.faceAttributes.facialHair.sideburns > 0) {
+                console.log("FacialHair: Yes");
+            } else {
+                console.log("FacialHair: No");
+            }
+            console.log("Gender: " + face.faceAttributes.gender);
+            console.log("Glasses: " + face.faceAttributes.glasses);
 
-			// Get more attributes
-			console.log("Head pose:");
-			console.log("  Pitch: " + face.faceAttributes.headPose.pitch);
-			console.log("  Roll: " + face.faceAttributes.headPose.roll);
-			console.log("  Yaw: " + face.faceAttributes.headPose.yaw);
- 
-			console.log("Makeup: " + ((face.faceAttributes.makeup.eyeMakeup || face.faceAttributes.makeup.lipMakeup) ? "Yes" : "No"));
-			console.log("Noise: " + face.faceAttributes.noise.noiseLevel);
+            // Get hair color
+            var color = "";
+            if (face.faceAttributes.hair.hairColor.length === 0) {
+                if (face.faceAttributes.hair.invisible) { color = "Invisible"; } else { color = "Bald"; }
+            } else {
+                color = "Unknown";
+                var highest_confidence = 0.0;
+                face.faceAttributes.hair.hairColor.forEach(function(hair_color) {
+                    if (hair_color.confidence > highest_confidence) {
+                        highest_confidence = hair_color.confidence;
+                        color = hair_color.color;
+                    }
+                });
+            }
+            console.log("Hair: " + color);
 
-			console.log("Occlusion:");
-			console.log("  Eye occluded: " + (face.faceAttributes.occlusion.eyeOccluded ? "Yes" : "No"));
-			console.log("  Forehead occluded: " + (face.faceAttributes.occlusion.foreheadOccluded ? "Yes" : "No"));
-			console.log("  Mouth occluded: " + (face.faceAttributes.occlusion.mouthOccluded ? "Yes" : "No"));
+            // Get more attributes
+            console.log("Head pose:");
+            console.log("  Pitch: " + face.faceAttributes.headPose.pitch);
+            console.log("  Roll: " + face.faceAttributes.headPose.roll);
+            console.log("  Yaw: " + face.faceAttributes.headPose.yaw);
 
-			console.log("Smile: " + face.faceAttributes.smile);
+            console.log("Makeup: " + ((face.faceAttributes.makeup.eyeMakeup || face.faceAttributes.makeup.lipMakeup) ? "Yes" : "No"));
+            console.log("Noise: " + face.faceAttributes.noise.noiseLevel);
 
-			console.log("QualityForRecognition: " + face.faceAttributes.qualityForRecognition)
-			console.log();
-		});
-	}));
+            console.log("Occlusion:");
+            console.log("  Eye occluded: " + (face.faceAttributes.occlusion.eyeOccluded ? "Yes" : "No"));
+            console.log("  Forehead occluded: " + (face.faceAttributes.occlusion.foreheadOccluded ? "Yes" : "No"));
+            console.log("  Mouth occluded: " + (face.faceAttributes.occlusion.mouthOccluded ? "Yes" : "No"));
+
+            console.log("Smile: " + face.faceAttributes.smile);
+
+            console.log("QualityForRecognition: " + face.faceAttributes.qualityForRecognition)
+            console.log();
+        });
+    }));
 }
 
 async function DetectFaceRecognize(url) {
-    let detected_faces = await client.face.detectWithUrl(url,
-		{
-			detectionModel: "detection_03",
-			recognitionModel: "recognition_04",
-            returnFaceAttributes: ["QualityForRecognition"]
-		});
+    let detected_faces = await client.face.detectWithUrl(url, {
+        detectionModel: "detection_03",
+        recognitionModel: "recognition_04",
+        returnFaceAttributes: ["QualityForRecognition"]
+    });
     return detected_faces.filter(face => face.faceAttributes.qualityForRecognition == 'high' || face.faceAttributes.qualityForRecognition == 'medium');
 }
 // </recognize>
@@ -179,33 +176,33 @@ async function FindSimilar() {
     console.log("========FIND SIMILAR========");
     console.log();
 
-	const source_image_file_name = "1.jpg";
+    const source_image_file_name = "1.jpg";
     const target_image_file_names = [
-		"1.jpg",
-		"2.jpg",
-		"3.jpg",
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
         "4.jpg",
-		"5.jpg",
-		"6.jpg",
+        "5.jpg",
+        "6.jpg",
         "7.jpg",
-		"8.jpg",
-		
-	];
+        "8.jpg",
 
-	let target_face_ids = (await Promise.all (target_image_file_names.map (async function (target_image_file_name) {
+    ];
+
+    let target_face_ids = (await Promise.all(target_image_file_names.map(async function(target_image_file_name) {
         // Detect faces from target image url.
         var faces = await DetectFaceRecognize(image_base_url + target_image_file_name);
-		console.log(faces.length + " face(s) detected from image: " +  target_image_file_name + ".");
-        return faces.map (function (face) { return face.faceId });;
-	}))).flat();
+        console.log(faces.length + " face(s) detected from image: " + target_image_file_name + ".");
+        return faces.map(function(face) { return face.faceId });;
+    }))).flat();
 
     // Detect faces from source image url.
-	let detected_faces = await DetectFaceRecognize(source_image_base_url + source_image_file_name);
+    let detected_faces = await DetectFaceRecognize(source_image_base_url + source_image_file_name);
 
     // Find a similar face(s) in the list of IDs. Comapring only the first in list for testing purposes.
-    let results = await client.face.findSimilar(detected_faces[0].faceId, { faceIds : target_face_ids });
-	results.every (function (result) {
-		console.log("Faces from: " + source_image_file_name + " and ID: " + result.faceId + " are similar with confidence: " + result.confidence + ".");
+    let results = await client.face.findSimilar(detected_faces[0].faceId, { faceIds: target_face_ids });
+    results.every(function(result) {
+        console.log("Faces from: " + source_image_file_name + " and ID: " + result.faceId + " are similar with confidence: " + result.confidence + ".");
         ok = result.confidence > 0.9;
     });
 }
@@ -214,35 +211,37 @@ async function FindSimilar() {
 
 
 const getBlobName = (originalName) => {
-  //   const identifier = Math.random().toString().replace(/0\./, ""); // remove "0." from start of string
-  return '1.jpg';
+    //   const identifier = Math.random().toString().replace(/0\./, ""); // remove "0." from start of string
+    return '1.jpg';
 };
 
 router.post("/", uploadStrategy, (req, res) => {
-  const blobName = getBlobName(req.file.originalname),stream = getStream(req.file.buffer), streamLength = req.file.buffer.length;
-  blobService.createBlockBlobFromStream(
-    containerName,
-    blobName,
-    stream,
-    streamLength,
-    async (err) => {
-      if (err) {
-        handleError(err);
-        return;
-      }
+    const blobName = getBlobName(req.file.originalname),
+        stream = getStream(req.file.buffer),
+        streamLength = req.file.buffer.length;
+    blobService.createBlockBlobFromStream(
+        containerName,
+        blobName,
+        stream,
+        streamLength,
+        async(err) => {
+            if (err) {
+                handleError(err);
+                return;
+            }
 
-    await FindSimilar();
-    if (ok){
-      res.render("success", {
-        message: "Image Check.",
-      });
-    } else {
-        res.render("error", {
-            message: "Image Not Check.",
-        });
-    }
-    }
-  );
+            await FindSimilar();
+            if (ok) {
+                res.render("success", {
+                    message: "Image Check.",
+                });
+            } else {
+                res.render("error", {
+                    message: "Image Not Check.",
+                });
+            }
+        }
+    );
 });
 
 module.exports = router;
--- routes/upload2.js
+++ routes/upload2.js
@@ -2,85 +2,79 @@ const { QueueClient } = require("@azure/storage-queue");
 const uuidv1 = require("uuidv1");
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").load();
+    require("dotenv").load();
 }
 const express = require("express"),
-  router = express.Router(),
-  multer = require("multer"),
-  inMemoryStorage = multer.memoryStorage(),
-  uploadStrategy = multer({ storage: inMemoryStorage }).fields([
-      {messagetxt: "messagetxt"},
-      {name: 'name'}]),
-  azureStorage = require("azure-storage"),
-  blobService = azureStorage.createBlobService(),
-  getStream = require("into-stream"),
-  containerName = "images",
-  http = require("http");
+    router = express.Router(),
+    multer = require("multer"),
+    inMemoryStorage = multer.memoryStorage(),
+    uploadStrategy = multer({ storage: inMemoryStorage }).fields([
+        { messagetxt: "messagetxt" },
+        { name: 'name' }
+    ]),
+    azureStorage = require("azure-storage"),
+    blobService = azureStorage.createBlobService(),
+    getStream = require("into-stream"),
+    containerName = "images",
+    http = require("http");
 const handleError = (err, res) => {
-  res.status(500);
-  res.render("error", { error: err });
+    res.status(500);
+    res.render("error", { error: err });
 };
 
-const AZURE_STORAGE_CONNECTION_STRING='DefaultEndpointsProtocol=https;AccountName=sakardesler;AccountKey=uRAgn/Mm8uoHkHGbfqwihNPPST537Yx26Vds4WTkMss7eW8zqfmUjYipBounIigUYOqKh5Ixvk6afMqZqsamPg==;EndpointSuffix=core.windows.net'
+const AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=sakardeser;AccountKey=a628iIIFFJL9OymcPzfBJPbdP8/zdChqqIvva0AJkwfK5jhZTmrA9eE90Gz+43KnyJP9GXS+H9hvFv9bQTSBiQ==;EndpointSuffix=core.windows.net'
 
-console.log(AZURE_STORAGE_CONNECTION_STRING);
+// console.log(AZURE_STORAGE_CONNECTION_STRING);
 
 let peekedMessages;
 
 async function main(msg) {
     console.log("Azure Queue Storage client library v12 - JavaScript quickstart sample");
-    
-// Create a unique name for the queue
-const queueName = "todoqueue" //+ uuidv1();
 
-console.log("\nCreating queue...");
-console.log("\t", queueName);
+    // Create a unique name for the queue
+    const queueName = "todoqueue" //+ uuidv1();
 
-// Instantiate a QueueClient which will be used to create and manipulate a queue
-const queueClient = new QueueClient(AZURE_STORAGE_CONNECTION_STRING, queueName);
+    console.log("\nCreating queue...");
+    console.log("\t", queueName);
 
-// Create the queue
-const createQueueResponse = await queueClient.create();
-console.log("Queue created, requestId:", createQueueResponse.requestId);
+    // Instantiate a QueueClient which will be used to create and manipulate a queue
+    const queueClient = new QueueClient(AZURE_STORAGE_CONNECTION_STRING, queueName);
 
-console.log("\nAdding messages to the queue...");
+    // Create the queue
+    const createQueueResponse = await queueClient.create();
+    console.log("Queue created, requestId:", createQueueResponse.requestId);
 
-// Send several messages to the queue
-// await queueClient.sendMessage(msg);
-// await queueClient.sendMessage("Second message");
-const sendMessageResponse = await queueClient.sendMessage(msg);
+    console.log("\nAdding messages to the queue...");
 
-console.log("Messages added, requestId:", sendMessageResponse.requestId);
+    // Send several messages to the queue
+    // await queueClient.sendMessage(msg);
+    // await queueClient.sendMessage("Second message");
+    const sendMessageResponse = await queueClient.sendMessage(msg);
 
-console.log("\nPeek at the messages in the queue...");
+    console.log("Messages added, requestId:", sendMessageResponse.requestId);
 
-// Peek at messages in the queue
-peekedMessages = await queueClient.peekMessages({ numberOfMessages : 32 });
+    console.log("\nPeek at the messages in the queue...");
 
-for (  i = 0; i < peekedMessages.peekedMessageItems.length-1;  i++) {
-    // Display the peeked message
-    console.log("\t", peekedMessages.peekedMessageItems[i].messageText);
- 
+    // Peek at messages in the queue
+    peekedMessages = await queueClient.peekMessages({ numberOfMessages: 32 });
+
+    for (i = 0; i < peekedMessages.peekedMessageItems.length - 1; i++) {
+        // Display the peeked message
+        console.log("\t", peekedMessages.peekedMessageItems[i].messageText);
+
+    }
 }
-}
 
 
 
 
 
-
-router.post("/", uploadStrategy, async (req, res) => {
+router.post("/", uploadStrategy, async(req, res) => {
     // console.log(req.body.messagetxt)
     // console.log(req.body.name)
-    await main(req.body.name + ' : ' + req.body.messagetxt).then(() => console.log("\nDone")).catch((ex) => console.log(ex.message));
+    await main(req.body.name + ' : ' + req.body.messagetxt).then(() => res.redirect(`https://fa-kardesler.azurewebsites.net/api/HttpTrigger2?name=${req.body.name}:${req.body.messagetxt}`)).catch((ex) => console.log(ex.message));
     var x = peekedMessages;
-    
-    // console.log(x.peekedMessageItems[1].messageText);
-    // res.render('group', x)
-    // console.log('------')
-    // console.log(x);
+
 });
 
 module.exports = router;
-
-
